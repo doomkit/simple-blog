577b992cd9d54e84c86c8b4d3a9d062681977ad5
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-11-05 23:37:53 +0100

    Add: redis, express-session to app

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,15 @@ import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import mikroOrmConfig from './mikro-orm.config';
 import express from 'express';
-import { __port__ } from './constants';
+import { __port__, __prod__, __redis__ } from './constants';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { PostResolver, UserResolver } from './resolvers';
+
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { MyContext } from './types';
 
 const server = async () => {
 	const orm = await MikroORM.init(mikroOrmConfig);
@@ -16,12 +21,35 @@ const server = async () => {
 		res.send('Hello World!');
 	});
 
+	const RedisStore = connectRedis(session);
+	const redisClient = redis.createClient();
+
+	if (!__redis__.secret) {
+		throw new Error("Can't connect to redis: secret is undefined");
+	}
+
+	app.use(
+		session({
+			name: 'sbid',
+			store: new RedisStore({ client: redisClient, disableTouch: true }),
+			cookie: {
+				maxAge: 1000 * 86400 * 365, // 1 year
+				httpOnly: true,
+				sameSite: 'lax',
+				secure: __prod__,
+			},
+			saveUninitialized: false,
+			secret: __redis__.secret,
+			resave: false,
+		})
+	);
+
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
 			resolvers: [PostResolver, UserResolver],
 			validate: false,
 		}),
-		context: () => ({ em: orm.em }),
+		context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
 	});
 
 	apolloServer.applyMiddleware({ app });
