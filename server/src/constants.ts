1aecb0e5a61b1b9f2ae2105c99a15e3b0ee4e61d
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-11-05 23:34:55 +0100

    🐰 Add: redis container

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,7 +1,4 @@
-import path from 'path';
-require('dotenv').config({
-	path: path.resolve(process.cwd(), '.env.postgres'),
-});
+require('dotenv').config();
 
 export const __prod__ = process.env.NODE_ENV !== 'production';
 export const __port__ = 3000;
@@ -11,4 +8,8 @@ export const __db__ = {
 	port: Number(process.env.POSTGRES_PORT),
 	user: process.env.POSTGRES_USER,
 	password: process.env.POSTGRES_PASSWORD,
+};
+
+export const __redis__ = {
+	secret: process.env.REDIS_SECRET,
 };
